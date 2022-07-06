1bb81fb639770a4e7d45db5cc74f759ed1188679
tylergubser <tylergubser2200@gmail.com>
2022-07-05 21:59:28 -0500

    migrated

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,8 @@ ActiveRecord::Schema.define(version: 2022_06_01_132058) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "password_digest"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
