d18aa5b57abff57c80b0f2815ae22d9b09629e6a
tylergubser <tylergubser2200@gmail.com>
2022-07-10 21:50:10 -0500

    show post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,5 +3,10 @@ class PostsController < ApplicationController
     def index
         render json: Post.all
     end
+
+    def show
+        post = Post.find(params[:id])
+        render json: post
+    end
     
 end
