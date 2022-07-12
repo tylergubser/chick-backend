63a29a475e121f346d7e1b9842f960f710cf344b
tylergubser <tylergubser2200@gmail.com>
2022-07-11 22:32:59 -0500

    user create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,9 @@ class UsersController < ApplicationController
     def index 
         render json: User.all 
     end
+
+    def create
+        user = User.find(params[:id])
+        render json: user, status: :created
+    end
 end
