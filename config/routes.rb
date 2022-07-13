2aff40b7b56fb2414b316dd5ebfcfb2eb3715333
tylergubser <tylergubser2200@gmail.com>
2022-07-12 22:04:17 -0500

    added posts routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  resources users, only: [:index, :show, :destroy, :create]
+  resources :users, only: [:index, :show, :destroy, :create]
+  resources :posts, only: [:index, :show, :destroy, :create]
 end
